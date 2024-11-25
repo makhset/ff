41299b4acaa20b95dc5ca53841e3878a381d2954
makhset <beibarysm10@gmail.com>
2024-11-25 17:54:35 +0500

    Add files via upload

---
+++ Emtihan5.js
@@ -0,0 +1,81 @@
+//tapsyrma 1
+/* function biba(){
+    while(true){
+    let san1 = +prompt("1 shy san")
+    let san2 = +prompt("2 shy san")
+    let aiyrma = prompt("aiyrma jaz; +, -, /, *")
+    if(aiyrma == "+"){
+        alert(san1 + san2)
+    }else if(aiyrma == "-"){
+        alert(san1 - san2)
+    }else if(aiyrma == "*"){
+        alert(san1 * san2)
+    }else if(aiyrma == "/"){
+        alert(san1 / san2)
+    }else if(san1 == ""){
+        alert("kate")
+    }else{
+        alert("kate")
+    }
+    }
+}
+biba(); */
+
+
+//Tapsyma 2
+// function skidka (){
+//     let price = parseFloat(prompt("Заттың бағасын енгізіңіз:"));
+//     let quantity = parseInt(prompt("Қанша зат сатып алғыңыз келеді?"));
+    
+    
+//     let totalCost = price * quantity;
+    
+//     let discount = 0;
+    
+//     if (totalCost > 20000) {
+//         discount = 0.3; 
+//     } else if (totalCost > 10000) {
+//         discount = 0.2; 
+//     } else if (totalCost > 5000) {
+//         discount = 0.1; 
+//     }
+//     let discountAmount = totalCost * discount;
+//     let finalAmount = totalCost - discountAmount;
+    
+    
+//     alert(`jalpy kunu: ${totalCost.toFixed(2)} tenge
+//     jenildik: ${discountAmount.toFixed(2)} tenge
+//     : ${finalAmount.toFixed(2)} tenge`);
+//     }
+//     skidka();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
